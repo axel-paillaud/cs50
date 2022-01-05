b05160b8f326c42d2d2f5a72f1d3d69ea3189d0a
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-05 05:55:45 +0000

    Wed, Jan 5, 2022, 5:55 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,6 +5,11 @@ coins = 0
 while(change < 0):
     change = get_float("Change owed: ")
 
-
+if(change >= 0.25):
+    n = change / 0.25
+    round(n)
+    for i in range(n):
+        coins+= 1
+        change -= 0.25
 
 print(coins)
