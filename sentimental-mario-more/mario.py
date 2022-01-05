d3be0e9d81b2763808cc2a60f4fd70b6503470e1
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-05 04:47:27 +0000

    Wed, Jan 5, 2022, 4:47 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -4,10 +4,10 @@ n = 999
 while(n < 1 or n > 8):
     n = get_int("Height: ")
 
-
+space = n - 1
 for i in range(n):
     for j in range(i + 1):
-        for k in range(n - (i + 1)):
+        for k in range(space):
             print(" ", end="")
         print("#", end="")
     print()
