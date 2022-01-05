53a4c992fbdf139604ba1450a973096167b50e72
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-05 05:05:51 +0000

    Wed, Jan 5, 2022, 5:05 AM +00:00

---
+++ sentimental-mario-less/mario.py
@@ -0,0 +1,18 @@
+from cs50 import get_int
+
+n = 999
+while(n < 1 or n > 8):
+    n = get_int("Height: ")
+
+space = n - 1
+hash = False
+for i in range(n):
+    for j in range(i + 1):
+        for k in range(space):
+            if hash == False:
+                print(" ", end="")
+        print("#", end="")
+        hash = True
+    hash = False
+    space -= 1
+    print()
