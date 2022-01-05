e867fe4f8ac4205e9e9517717094b29f966ef6b7
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-05 05:30:46 +0000

    Wed, Jan 5, 2022, 5:30 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,7 +1,9 @@
 from cs50 import get_float
 
 change = -1
+coins = 
 while(change < 0):
     change = get_float("Change owed: ")
 
-for i in range(change)
+for i in range(change):
+    if(change > 0.25):
