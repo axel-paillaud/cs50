e329b32726505903cd8e5d81dad6357c8eb7d58e
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-06 05:04:16 +0000

    Thu, Jan 6, 2022, 5:04 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,12 +3,14 @@ from cs50 import get_string
 def main():
 
     s = get_string("Text: ")
+    count_letters(s)
 
 def count_letters(sentences):
     count_letters = 0
     for i in range(len(sentences)):
-        
+        c = sentences[i]
+        print(c)
 
-if __name__ "__main__":
+if __name__ == "__main__":
     main()
 
