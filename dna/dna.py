d6b95758a4ebba5803435c8381b7c526bddbcf94
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-06 08:16:15 +0000

    Thu, Jan 6, 2022, 8:16 AM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(sys.argv != 2):
+    if len(sys.argv) != 2:
         sys.exit("Usage: python dna.py [datafile][sequence]")
 
     # TODO: Read database file into a variable
