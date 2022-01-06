b7adab471e9c801b6f485c4e88ebb2df78c56e59
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-06 12:55:19 +0000

    Thu, Jan 6, 2022, 12:55 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -17,7 +17,7 @@ def main():
         for valeur in row.values():
             if valeur.isdigit() == True:
                 valeur = int(valeur)
-                row.update(valeur)
+                row[""] = valeur
 
         print(row)
         data.append(row)
