967d9cab2a3d170de83f799efcdfcb287f08356c
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-06 15:30:17 +0000

    Thu, Jan 6, 2022, 3:30 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -20,7 +20,7 @@ def main():
         for column in row.values():
             if column.isdigit() == True:
                 valueint = int(column)
-                print
+                print(data[1])
 
 
     # TODO: Read DNA sequence file into a variable
