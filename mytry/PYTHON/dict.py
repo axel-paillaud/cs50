29af26f9dcc2ca222b94e01717d1834607d12407
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-07 10:38:16 +0000

    Fri, Jan 7, 2022, 10:38 AM +00:00

--- mytry/PYTHON/dict.py
+++ mytry/PYTHON/dict.py
@@ -4,7 +4,15 @@ print(dict["Axel"])
 
 list = list(dict.values())
 
+print(list[0:2])
+
+list[0] = int(list[0])
+
+list[1] = int(list[1])
+
+sum = list[0] + list[1]
+
 print(list)
 
-print(list[0:2])
+print(sum)
 
