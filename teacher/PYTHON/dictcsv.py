533a9d2c9dc77b5d5bb7b307878048e748580b9a
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-07 15:32:06 +0000

    Fri, Jan 7, 2022, 3:32 PM +00:00

--- teacher/PYTHON/dictcsv.py
+++ teacher/PYTHON/dictcsv.py
@@ -5,5 +5,9 @@ titles = []
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
-        if not row["title"] in titles:
-            titles.append(row["title"])
+        title = row["title"].strip().uppercse()
+        if not title in titles:
+            titles.append(title)
+
+for title in titles:
+    print(title)
