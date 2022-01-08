010214dccf6e996cc3d0825fedbaca3f45f4e521
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-08 12:41:10 +0000

    Sat, Jan 8, 2022, 12:41 PM +00:00

--- training/PYTHON/data.py
+++ training/PYTHON/data.py
@@ -1,10 +1,13 @@
 import csv
 import sys
+import os
+import os.path
 
 if len(sys.argv) != 2:
     print("Usage: data.py [name of the csv file]")
     sys.exit
 
+print(os.listdir())
 input = sys.argv[1]
 
 with open(input, 'r') as file:
