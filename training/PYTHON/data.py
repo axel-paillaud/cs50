b104e5b0ce476c8a94bff2a4c9462ba2a5d70fff
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-08 12:55:24 +0000

    Sat, Jan 8, 2022, 12:55 PM +00:00

--- training/PYTHON/data.py
+++ training/PYTHON/data.py
@@ -1,14 +1,11 @@
 import csv
 import sys
-import os
-import os.path
+
 
 if len(sys.argv) != 2:
     print("Usage: data.py [name of the csv file]")
     sys.exit
 
-input = sys.argv[1]
-print(input)
 
-with open(input, 'r') as file:
+with open(sys.argv[1], 'r') as file:
     reader = csv.DictReader(file)
