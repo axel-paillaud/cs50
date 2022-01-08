4da986fb0083db6b621539812e0bdf438c1140eb
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-08 14:58:18 +0000

    Sat, Jan 8, 2022, 2:58 PM +00:00

--- training/PYTHON/data.py
+++ training/PYTHON/data.py
@@ -18,7 +18,7 @@ while True:
     dir_count = 0
 
     for file in file_list:
-        if isdir(file) == True:
+        if os.path.isdir(cur_dir + "/" + file) == True:
             dir_count += 1
 
     print(dir_count)
