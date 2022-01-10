f70d7ac16a8dbfffb95838ac204a8811b6aab597
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-10 07:27:33 +0000

    Mon, Jan 10, 2022, 7:27 AM +00:00

---
+++ training/PYTHON/search_file.py
@@ -0,0 +1,61 @@
+import csv
+import sys
+import os
+import os.path
+
+
+if len(sys.argv) != 2:
+    print("Usage: data.py [name of the csv file]")
+    sys.exit
+
+input = sys.argv[1]
+cur_dir = os.getcwd() # Dossier actuel
+
+find = False
+while True:
+
+    file_list = os.listdir(cur_dir) # Liste des fichiers du dossier actuel
+    dir_count = 0
+    tmp = 0
+
+    for file in file_list:
+        file_path = cur_dir + "/" + file
+
+        if file == "root":
+            sys.exit("Fichier non trouvé")
+        if os.path.isdir(file_path) == True:
+            dir_count += 1
+
+
+    parent_dir = os.path.dirname(cur_dir) # Dossier parent par rapport au dossier actuel
+
+    if input in file_list:
+        print("Fichier trouvé")
+        break
+    else:
+        while tmp != dir_count:
+            if find == True:
+                break
+            for file in file_list:
+                file_path = cur_dir + "/" + file
+                if os.path.isdir(file_path) == True:
+                    tmp += 1
+                    list_search = os.listdir(file_path)
+                    if input in list_search:
+                        print("Fichier trouvé")
+                        find = True
+                        break
+
+    if find == True:
+        break
+    elif cur_dir == parent_dir: # Si le dossier actuel est le dossier root
+        sys.exit("Fichier non trouvé")
+    else:
+        cur_dir = parent_dir
+
+if find == True:
+    input = file_path + "/" + input
+else:
+    input = cur_dir + "/" + input
+
+
