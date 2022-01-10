0c598d497c7f5d2a0a11d2353a9402b3e2a4c431
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-10 07:36:43 +0000

    Mon, Jan 10, 2022, 7:36 AM +00:00

--- training/PYTHON/search_file.py
+++ training/PYTHON/search_file.py
@@ -14,33 +14,35 @@ cur_dir = os.getcwd() # Dossier actuel
 find = False
 while True:
 
-    file_list = os.listdir(cur_dir) # Liste des fichiers du dossier actuel
+    file_list = os.listdir(cur_dir)         # Liste des fichiers du dossier actuel
     dir_count = 0
     tmp = 0
 
     for file in file_list:
         file_path = cur_dir + "/" + file
 
-        if file == "root":
+        if file == "root":                  # Pour éviter d'aller chercher dans le dossier "root" et d'avoir un message d'erreur "acces denied"
             sys.exit("Fichier non trouvé")
         if os.path.isdir(file_path) == True:
             dir_count += 1
 
 
-    parent_dir = os.path.dirname(cur_dir) # Dossier parent par rapport au dossier actuel
+    parent_dir = os.path.dirname(cur_dir)   # Dossier parent par rapport au dossier actuel
 
     if input in file_list:
         print("Fichier trouvé")
         break
     else:
-        while tmp != dir_count:
+        while tmp != dir_count:             # Tant qu'on a pas fait tout les dossiers de la liste
             if find == True:
                 break
             for file in file_list:
                 file_path = cur_dir + "/" + file
+
                 if os.path.isdir(file_path) == True:
                     tmp += 1
-                    list_search = os.listdir(file_path)
+                    list_search = os.listdir(file_path)     #Liste des fichiers/dossiers dans le dossier dans lequel on cherche
+
                     if input in list_search:
                         print("Fichier trouvé")
                         find = True
@@ -48,14 +50,15 @@ while True:
 
     if find == True:
         break
-    elif cur_dir == parent_dir: # Si le dossier actuel est le dossier root
+    elif cur_dir == parent_dir:     # Si le dossier actuel est le dossier root
         sys.exit("Fichier non trouvé")
     else:
         cur_dir = parent_dir
 
-if find == True:
+if find == True:                    # Si on a trouvé le fichier dans les sous-dossiers, alors find == True, et il faut suivre le path suivant :
     input = file_path + "/" + input
-else:
+else:                               # Sinon, le fichier trouvé était dans le dossier actuel, et il faut prendre le path suivant :
     input = cur_dir + "/" + input
+
 
 
