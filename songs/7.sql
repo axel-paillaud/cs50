656f1d268a998195f42699a576ea1c8195ee26e8
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-14 05:39:02 +0000

    Fri, Jan 14, 2022, 5:39 AM +00:00

--- songs/7.sql
+++ songs/7.sql
@@ -1,1 +1,1 @@
-SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = "Post Malone");
+SELECT AVG(energy) FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = "Drake");
