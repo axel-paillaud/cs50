ef93b590e2a0406db88251668fc7629c5a487a22
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-15 06:02:16 +0000

    Sat, Jan 15, 2022, 6:02 AM +00:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies JOIN ratings ON movie_id = id WHERE year = 2010 ORDER BY rating, title;
+SELECT title, rating FROM movies JOIN ratings ON movie_id = id WHERE year = 2010 ORDER BY rating DESC, title;
