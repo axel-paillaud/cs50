b5de5678d9e6a50b2b98deec4a85a531ad8f7d19
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-17 09:23:56 +0000

    Mon, Jan 17, 2022, 9:23 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,16 +1,5 @@
 SELECT movies.title FROM movies JOIN stars, people
 ON stars.person_id = people.id AND stars.movie_id = movies.id
-WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
-
-SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id IN (SELECT people.name = "Johnny Depp") AND (SELECT people.name = "Helena Bonham Carter");
-
-SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter" IN people;
-
-SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Johnny Depp";
-
-
-SELECT movies.title FROM movies JOIN stars, people
-ON stars.person_id = people.id AND stars.movie_id = movies.id
 WHERE people.name = "Johnny Depp" INTERSECT
 SELECT movies.title FROM movies JOIN stars, people
 ON stars.person_id = people.id AND stars.movie_id = movies.id
