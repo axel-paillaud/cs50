20fc5c65ddbb464c5cfadaf800fd1f55b4dd4a0a
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-17 10:21:47 +0000

    Mon, Jan 17, 2022, 10:21 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,4 +4,4 @@ SELECT people.name from people JOIN stars, movies ON stars.person_id = people.id
 
 SELECT people.name from people JOIN stars, movies ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon";
 
-SELECT people.name FROM people JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE (SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon");
+SELECT people.name FROM people JOIN stars, movies ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE EXISTS (SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon");
