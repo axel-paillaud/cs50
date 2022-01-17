83a02440c413804578f48191aab2931216454c9e
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-17 09:26:13 +0000

    Mon, Jan 17, 2022, 9:26 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,6 +1,1 @@
-SELECT movies.title FROM movies JOIN stars, people
-ON stars.person_id = people.id AND stars.movie_id = movies.id
-WHERE people.name = "Johnny Depp" INTERSECT
-SELECT movies.title FROM movies JOIN stars, people
-ON stars.person_id = people.id AND stars.movie_id = movies.id
-WHERE people.name = "Helena Bonham Carter";
+SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Johnny Depp" INTERSECT SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Helena Bonham Carter";
