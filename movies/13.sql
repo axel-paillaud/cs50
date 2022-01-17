31376949e089f6bf52fa1431649b7dd5ec38a0c5
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-17 10:50:27 +0000

    Mon, Jan 17, 2022, 10:50 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -10,7 +10,7 @@ SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE movie_ID 
 
 SELECT * FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon";
 
-SELECT id FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon";
+SELECT movies.id FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon";
 
 SELECT people.name FROM people JOIN stars, movies ON stars.person_id = people.id AND stars.movie_ID = movies.id WHERE
 movies.id = (SELECT movies.id FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon");
