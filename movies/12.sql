0cb4a351fbab16af52740eb3e8f049ac3c84d448
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-17 09:12:34 +0000

    Mon, Jan 17, 2022, 9:12 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,6 @@
-SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
+SELECT movies.title FROM movies JOIN stars, people
+ON stars.person_id = people.id AND stars.movie_id = movies.id
+WHERE people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
 
 SELECT movies.title FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id IN (SELECT people.name = "Johnny Depp") AND (SELECT people.name = "Helena Bonham Carter");
 
