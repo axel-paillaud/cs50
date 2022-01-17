4c4a7f24752b8db1c68b1d386f715d59a8d4d1b7
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-17 10:45:48 +0000

    Mon, Jan 17, 2022, 10:45 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -12,4 +12,3 @@ SELECT * FROM movies JOIN stars, people ON stars.person_id = people.id AND stars
 
 SELECT id FROM movies JOIN stars, people ON stars.person_id = people.id AND stars.movie_id = movies.id WHERE people.name = "Kevin Bacon";
 
-
