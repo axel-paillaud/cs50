f3b9e7cced88da0218e23241294052a8f223e481
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-18 05:52:08 +0000

    Tue, Jan 18, 2022, 5:52 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Commande pour avoir la description des crimes ayant eu lieu le même jour, mois, année que le canard volé de CS50
+SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND year = 2021;
+
