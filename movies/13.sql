c9413210734452bfd9cae8cc67efd0d188855ae2
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-18 05:18:03 +0000

    Tue, Jan 18, 2022, 5:18 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -28,4 +28,4 @@ SELECT person_id FROM stars WHERE person_id IN (SELECT movie_id FROM stars WHERE
 
 SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE person_id IN (SELECT movie_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon")))));
 
-SELECT name FROM people WHERE id IN (SELECT person_id FROM stars WHERE person_id IN (SELECT movie_id FROM stars WHERE movie_id IN (SELECT id FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon")))));
+ SELECT person_id FROM stars WHERE person_id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Kevin Bacon"));
