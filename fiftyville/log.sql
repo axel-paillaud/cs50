8e62ff6fc617e18141f31c43c467778c11eded2c
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-18 06:03:33 +0000

    Tue, Jan 18, 2022, 6:03 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,3 +6,7 @@ SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND yea
 
 -- Avoir l'activité de la boulangerie le jour J
 SELECT activity, license_plate, hour, minute FROM bakery_security_logs WHERE year = 2021 AND month = 7 AND day = 28;
+
+
+-- Pour l'instant ne marche pas
+sqlite> SELECT name FROM people WHERE license_plate = "5P2BI95" || "94KL13X" || "6P58WS2";
