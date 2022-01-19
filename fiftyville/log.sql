d2a67c132aeee66f9e7aeb1128e0556196c52928
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-01-19 05:54:40 +0000

    Wed, Jan 19, 2022, 5:54 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -67,4 +67,7 @@ SELECT caller, receiver, day, month, year, duration FROM phone_calls WHERE calle
 SELECT caller, receiver, day, month, year, duration FROM phone_calls WHERE caller  = "(344) 555-9601";
 
 
-SELECT amount, transaction_type, atm_location, day, month, year FROM atm_transactions WHERE account_number IN (SELECT account_number FROM bank_accounts WHERE person_id IN (SELECT id FROM people WHERE name = "Bruce"));
+-- Pour avoir tout les retraits de la personne concerné, avec la date et le lieu.
+SELECT amount, transaction_type, atm_location, day, month, year FROM atm_transactions WHERE account_number IN
+    (SELECT account_number FROM bank_accounts WHERE person_id IN
+        (SELECT id FROM people WHERE name = "Bruce"));
