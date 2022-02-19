8c8b34af81952efa1e1b7d9011c28bacfa3b071a
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-02-19 05:19:17 +0000

    Sat, Feb 19, 2022, 5:19 AM +00:00

--- homepage/script/camionAnimation.js
+++ homepage/script/camionAnimation.js
@@ -1,15 +1,15 @@
 const camion1 = document.getElementById("camionInkAnim1");
 const camion2 = document.getElementById("camionInkAnim2");
 
-function moteur() {
-    var show1 = camion1.style.display;
-    var show2 = camion2.style.display;
 
-    show1 == "block";
-    show2 == "none";
-    console.log(show1);
-    console.log(show2);
-}
+const show1 = camion1.style.display;
+const show2 = camion2.style.display;
+
+show1 == "block";
+show2 == "none";
+console.log(show1);
+console.log(show2);
+
 
 console.log(camion1);
-moteur();
+
