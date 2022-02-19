80372578745b53357571e6a4a79ff4573b2788cf
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-02-19 05:07:27 +0000

    Sat, Feb 19, 2022, 5:07 AM +00:00

--- homepage/script/camionAnimation.js
+++ homepage/script/camionAnimation.js
@@ -2,6 +2,9 @@ const camion1 = document.getElementById("camionInkAnim1");
 const camion2 = document.getElementById("camionInkAnim2");
 
 function moteur() {
+    var show1 = camion1.style.display;
+    var show2 = camion2.style.display;
+
     
 }
 
