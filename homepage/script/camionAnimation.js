e7084d4165ba36c6d0113f9b3153d073ccacb5ff
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-02-21 06:46:26 +0000

    Mon, Feb 21, 2022, 6:46 AM +00:00

--- homepage/script/camionAnimation.js
+++ homepage/script/camionAnimation.js
@@ -3,17 +3,23 @@ const camion2 = document.getElementById("camionInkAnim2");
 camion1.style.display = "block";
 camion2.style.display = "none";
 
-function moteur(anim1, anim2) {
+let nIntervId;
 
-    if (anim1.style.display == "block") {
-        anim1.style.display = "none";
-        anim2.style.display = "block";
+function animCamion() {
+    
+}
+
+function switchAnim() {
+
+    if (camion1.style.display == "block") {
+        camion1.style.display = "none";
+        camion2.style.display = "block";
         }
 
     else {
-        anim1.style.display = "block";
-        anim2.style.display = "none";
+        camion1.style.display = "block";
+        camion2.style.display = "none";
         }
     }
 
-window.setInterval(moteur, 500);
+
