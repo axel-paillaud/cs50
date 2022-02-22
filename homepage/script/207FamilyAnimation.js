cfa9e90ec2aa9397381235930292636727249969
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-02-22 05:39:41 +0000

    Tue, Feb 22, 2022, 5:39 AM +00:00

---
+++ homepage/script/207FamilyAnimation.js
@@ -0,0 +1,13 @@
+const familyStand = document.getElementById("207FamilyStand");
+const familyAnim1 = document.getElementById("207FamilyAnim1");
+const familyAnim2 = document.getElementById("207FamilyAnim2");
+const familyAnim3 = document.getElementById("207FamilyAnim3");
+const camionAnim1 = document.getElementById("camionCote1");
+const camionAnim2 = document.getElementById("camionCote2");
+
+familyStand.style.visibility = "visible";
+familyAnim1.style.visibility = "hidden";
+familyAnim2.style.visibility = "hidden";
+familyAnim3.style.visibility = "hidden";
+camionAnim1.style.visibility = "hidden";
+camionAnim2.style.visibility = "hidden";
