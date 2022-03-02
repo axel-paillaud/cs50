2ae17d65b28011ee76c4c64fdbf663337a33b5f2
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-02 06:12:25 +0000

    Wed, Mar 2, 2022, 6:12 AM +00:00

--- homepage/script/menu_slide.js
+++ homepage/script/menu_slide.js
@@ -1,15 +1,15 @@
 const mapEuropeSvg = document.getElementById("map_of_europe");
-var slideContainer = document.getElementById("slide_container");
+var slideContainerMap = document.getElementById("slide_container_map");
 
 mapEuropeSvg.style.visibility = "hidden";
 
 function showOnOver() {
-    slideContainer.classList.add("slide_transition_in");
+    slideContainerMap.classList.add("slide_transition_in");
     mapEuropeSvg.style.visibility = "visible";
-    slideContainer.classList.remove("slide_transition_out");
+    slideContainerMap.classList.remove("slide_transition_out");
 }
 
 function hideOnOut() {
-    slideContainer.classList.remove("slide_transition_in");
-    slideContainer.classList.add("slide_transition_out");
+    slideContainerMap.classList.remove("slide_transition_in");
+    slideContainerMap.classList.add("slide_transition_out");
 }
