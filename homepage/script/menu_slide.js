43ab2744f4e04a4c7dcfe90a51713bab7f959061
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-02 06:36:06 +0000

    Wed, Mar 2, 2022, 6:36 AM +00:00

--- homepage/script/menu_slide.js
+++ homepage/script/menu_slide.js
@@ -1,15 +1,29 @@
 const mapEuropeSvg = document.getElementById("map_of_europe");
+const lightBulb = document.getElementById("light_bulb");
+var slideContainerTips = document.getElementById("slide_container_tips");
 var slideContainerMap = document.getElementById("slide_container_map");
 
 mapEuropeSvg.style.visibility = "hidden";
+lightBulb.style.visibility = "hidden";
 
-function showOnOver() {
+function showOnOverMap() {
     slideContainerMap.classList.add("slide_transition_in");
     mapEuropeSvg.style.visibility = "visible";
     slideContainerMap.classList.remove("slide_transition_out");
 }
 
-function hideOnOut() {
+function hideOnOutMap() {
     slideContainerMap.classList.remove("slide_transition_in");
     slideContainerMap.classList.add("slide_transition_out");
+}
+
+function showOnOverBulb() {
+    slideContainerTips.classList.add("slide_transition_in");
+    lightBulb.style.visibility = "visible";
+    slideContainerTips.classList.remove("slide_transition_out");
+}
+
+function hideOnOutBulb() {
+    slideContainerTips.classList.remove("slide_transition_in");
+    slideContainerTips.classList.add("slide_transition_out");
 }
