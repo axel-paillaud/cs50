685b271e53d718b8c7e580a756290f4bf45a0dc1
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-02 05:23:03 +0000

    Wed, Mar 2, 2022, 5:23 AM +00:00

--- homepage/script/menu_slide.js
+++ homepage/script/menu_slide.js
@@ -1,8 +1,9 @@
 const mapEuropeSvg = document.getElementById("map_of_europe");
 
-//mapEuropeSvg.style.visibility = "hidden";
+mapEuropeSvg.style.visibility = "hidden";
 
 function showOnOver() {
+    
     mapEuropeSvg.style.visibility = "visible";
 }
 
