5ef00af66e5c4fba32676cf23233e9426512de6a
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-03 05:23:12 +0000

    Thu, Mar 3, 2022, 5:23 AM +00:00

--- homepage/script/menu_slide.js
+++ homepage/script/menu_slide.js
@@ -1,13 +1,16 @@
 const mapEuropeSvg = document.getElementById("map_of_europe");
 const lightBulb = document.getElementById("light_bulb");
 const camionSvg = document.getElementById("camion_slide");
+const infoSvg = document.getElementById("info_svg");
 var slideContainerTips = document.getElementById("slide_container_tips");
 var slideContainerMap = document.getElementById("slide_container_map");
 var slideContainerCamion = document.getElementById("slide_container_camion");
+var slideContainerInfo = document.getElementById("slide_container_info");
 
 mapEuropeSvg.style.visibility = "hidden";
 lightBulb.style.visibility = "hidden";
 camionSvg.style.visibility = "hidden";
+infoSvg.style.visibility = "hidden";
 
 function showOnOverMap() {
     slideContainerMap.classList.add("slide_transition_in");
@@ -41,3 +44,4 @@ function hideOnOutCamion() {
     slideContainerCamion.classList.remove("slide_transition_in");
     slideContainerCamion.classList.add("slide_transition_out");
 }
+
