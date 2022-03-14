ed13eca3d7177de889f284ca7385d2a8176b3665
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-14 08:26:09 +0000

    Mon, Mar 14, 2022, 8:26 AM +00:00

--- teacher/Flask/froshims/app.py
+++ teacher/Flask/froshims/app.py
@@ -11,4 +11,6 @@ def index():
 @app.route("/greet")
 def greet():
 
-    # 
+    # Valider l'inscription
+    if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]
+        return render_template("failure.html")
