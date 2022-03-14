f4569cd388e3944c5aebffc4789d4600ae26f2b5
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-14 08:57:54 +0000

    Mon, Mar 14, 2022, 8:57 AM +00:00

--- teacher/Flask/froshims/app.py
+++ teacher/Flask/froshims/app.py
@@ -10,7 +10,7 @@ SPORTS = [
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sports=SPORTS)
 
 
 @app.route("/register", methods=["POST"])
