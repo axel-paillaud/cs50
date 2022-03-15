434482b022eec53893c9c36dca77161b75e84e06
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-15 04:33:15 +0000

    Tue, Mar 15, 2022, 4:33 AM +00:00

---
+++ teacher/Flask/froshims2/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+INSCRITS = {}
+
+SPORTS = [
+    "Basketball",
+    "Football",
+    "Soccer",
+    "Ultimate Frisbee"
+]
+
+@app.route("/")
+def index():
+    return render_template("index.html", sports=SPORTS)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    # Valider l'inscription
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+        return render_template("failure.html")
+
+    #Confirmer l'inscription
+    return render_template("succes.html")
