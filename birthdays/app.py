37c3c36956d74b37da8af6a1ee3d847e3623ccee
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-16 05:56:10 +0000

    Wed, Mar 16, 2022, 5:56 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -32,7 +32,7 @@ def index():
         month = request.form.get("month")
 
         if name and day and month:
-            db.execute()
+            db.execute("INSERT INTO birthdays(name, month, day) VALUE(?)" )
 
         return redirect("/")
 
