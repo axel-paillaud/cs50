759aa5b76524543f968d0ebdbd17fcb312db8d2b
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-16 05:33:07 +0000

    Wed, Mar 16, 2022, 5:33 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -33,7 +33,7 @@ def index():
     else:
 
         # TODO: Display the entries in the database on index.html
-        
+        births = db.execute("SELECT * FROM birthdays")
 
         return render_template("index.html")
 
