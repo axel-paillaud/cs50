4908a85f865edcf306dc7b521be1532878942e18
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-16 06:41:51 +0000

    Wed, Mar 16, 2022, 6:41 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -34,7 +34,10 @@ def index():
         if name and day and month:
             db.execute("INSERT INTO birthdays(name, month, day) VALUES(?, ?, ?)", name, month, day)
 
+        delete = request.form.get("delete")
 
+        if delete:
+            db.execute("DELETE FROM birthdays WHERE name=?", delete)
 
         return redirect("/")
 
