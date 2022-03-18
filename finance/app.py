7e2f819ade7ef30857a615f582fcf4b2f8a6a0b7
Shaun-maker <85078268+Shaun-maker@users.noreply.github.com>
2022-03-18 04:46:30 +0000

    Fri, Mar 18, 2022, 4:46 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -125,9 +125,8 @@ def register():
         elif not request.form.get("regPassword"):
             return apology("Must provide passowrd", 403)
 
-        print(request.form.get("regPassowrd") != request.form.get("regConfirm"))
-
         elif (request.form.get("regPassowrd")) != (request.form.get("regConfirm")):
+            print(request.form.get("regPassowrd") != request.form.get("regConfirm"))
             return apology("The confirmation is incorrect", 403)
 
         password = generate_password_hash(request.form.get("regPassword"))
